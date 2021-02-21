e35ef18860987287529b837a120b8e370b18a3e5
Will Bridges <will.p.bridges@gmail.com>
2021-02-21 01:49:41 -0600

    finished hw05

---
+++ hw05/bulls/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4790
+
+CFGD=$(readlink -f ~/.config/bulls)
+
+if [ ! -e "$CFGD/base" ]; then
+    echo "Need to deploy first"
+    exit 1
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/practice/bin/practice stop || true
+
+echo "Starting app..."
+
+_build/prod/rel/bulls/bin/bulls start
