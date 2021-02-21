d6bc905ca3cab9563407b675f14c4da0e2dcd089
Will Bridges <will.p.bridges@gmail.com>
2021-02-21 02:27:44 -0600

    fixed scripts

--- hw05/bulls/deploy.sh
+++ hw05/bulls/deploy.sh
@@ -3,23 +3,25 @@
 export SECRET_KEY_BASE=insecure
 export MIX_ENV=prod
 export PORT=4791
-export NODEBIN=`pwd`/assets/node_modules/.bin
-export PATH="$PATH:$NODEBIN"
 
-echo "Building..."
-
-mix deps.get
+mix deps.get --only prod
 mix compile
-(cd assets && npm install)
-(cd assets && webpack --mode production)
+
+CFGD=$(readlink -f ~/.config/bulls)
+
+if [ ! -d "$CFGD" ]; then
+    mkdir -p $CFGD
+fi
+
+if [ ! -e "$CFGD/base" ]; then
+    mix phx.gen.secret > "$CFGD/base"
+fi
+
+SECRET_KEY_BASE=$(cat "$CFGD/base")
+export SECRET_KEY_BASE
+
+npm install --prefix ./assets
+npm run deploy --prefix ./assets
 mix phx.digest
 
-echo "Generating release..."
 mix release
-
-#echo "Stopping old copy of app, if any..."
-#_build/prod/rel/bulls/bin/bulls stop || true
-
-echo "Starting app..."
-
-PROD=t ./start.sh
