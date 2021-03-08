be0a8c245c10dbeb69d6f4a7646b890c9114abf1
Will Bridges <will.p.bridges@gmail.com>
2021-03-07 18:01:11 -0600

    added prod database

--- hw07/events/config/prod.exs
+++ hw07/events/config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :events, EventsWeb.Endpoint,
   url: [host: "events.willbridges.website", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  database: "events_prod"
 
 # Do not print debug messages in production
 config :logger, level: :info
