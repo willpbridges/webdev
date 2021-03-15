8bdea46d8f890c4ae7235b7edea9644a94406863
Will Bridges <will.p.bridges@gmail.com>
2021-03-15 14:03:56 -0500

    basic comments working, invite system not implemented

--- hw07/README.md
+++ hw07/README.md
@@ -1,2 +1,6 @@
 All relevant code from Nat Tuck's Photo Blog lecture code
 was used as starter code for this assignment.
+
+Events part 2 adds code for comments and photo uploads,
+which was also taken from Nat Tuck's Photo Blog
+lecture code.
--- hw07/events/lib/events/comments.ex
+++ hw07/events/lib/events/comments.ex
@@ -19,6 +19,7 @@ defmodule Events.Comments do
   """
   def list_comments do
     Repo.all(Comment)
+    |> Repo.preload(:user)
   end
 
   @doc """
@@ -35,7 +36,10 @@ defmodule Events.Comments do
       ** (Ecto.NoResultsError)
 
   """
-  def get_comment!(id), do: Repo.get!(Comment, id)
+  def get_comment!(id) do
+    Repo.get!(Comment, id)
+    |> Repo.preload(:user)
+  end
 
   @doc """
   Creates a comment.
